b5f75fe1a6088a1391adbe05c09640d4f096bc85
Carlos Bido <carloshbido@gmail.com>
2020-09-08 21:33:01 -0300

    Added counter span

--- Cat-Facts/script.js
+++ Cat-Facts/script.js
@@ -1,13 +1,15 @@
 document.querySelector('#button').addEventListener('click', getSomething);
+let count = 0;
 
 function getSomething() {
     let xhr = new XMLHttpRequest();
 
+    //Open API
     xhr.open('GET','https://meowfacts.herokuapp.com/', true);
 
+    // Get Random text 
     xhr.onload = function() {
         if(this.status == 200) {
-
             let facts = JSON.parse(this.responseText);
             document.querySelector('.fact').innerHTML = facts.data;
         }
@@ -15,5 +17,12 @@ function getSomething() {
 
     xhr.send();
 
+
+    //Create Counter
+    count++;
+    let span = document.querySelector('span');
+    span.innerText = count;
+    
 } 
+
 
