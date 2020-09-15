c1207a23ca9a5e8cae51620b7302a8606608cea1
Carlos Bido <carloshbido@gmail.com>
2020-09-14 22:55:16 -0300

    Added new whiteColor

--- ColorsMouse/index.js
+++ ColorsMouse/index.js
@@ -1,6 +1,12 @@
 const square = document.querySelector('.square');
+
 square.addEventListener('mousemove', changeColor);
+square.addEventListener('mouseout', whiteColor);
 
 function changeColor(e) {
   document.body.style.backgroundColor = `rgb(${e.offsetX},${e.offsetY} ,40)`
+}
+
+function whiteColor(e) {
+  document.body.style.backgroundColor = 'white'
 }
