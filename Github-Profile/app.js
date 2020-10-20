7bd9b0f5085943a83c790d82a5fa867b86e506b7
Carlos Bido <carloshbido@gmail.com>
2020-10-20 14:21:09 -0300

    inserted field

--- Github-Profile/app.js
+++ Github-Profile/app.js
@@ -1,3 +1,7 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', (e) => {
   console.log('app carregado com sucesso');
 })
+
+document.querySelector('#user').addEventListener('keydown', (e) => {
+  console.log(e.target.value);
+});
