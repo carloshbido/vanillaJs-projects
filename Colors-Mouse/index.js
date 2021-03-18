8323c56d5aa54d6e8d9d51023dda64711dbc92ab
Carlos Bido <carloshbido@gmail.com>
2021-03-18 10:34:31 -0300

    improvements in the index.js

--- Colors-Mouse/index.js
+++ Colors-Mouse/index.js
@@ -2,33 +2,41 @@ const circle = document.querySelector('.circle');
 const header = document.querySelector('header textarea');
 
 function changeColor(e) {
+
   const blue = e.offsetX - e.offsetY < 0 ? 0 : e.offsetX - e.offsetY;
   const color = `rgb(${e.offsetX},${e.offsetY},${blue})`;
+  
   document.body.style.backgroundColor = color;
   header.innerHTML = color;
 
-  return color;
 }
 
 function copyColor(e){
-  const circleClicked = e.target.classList[0] === 'circle';
 
-  if (circleClicked) {
+  const circleClickedTwice = e.target.classList[0] === 'circle';
+
+  if (circleClickedTwice) {
   
-  const copyText = document.querySelector('header textarea');
+    const copyText = header;
+    
+    copyText.select();
+    copyText.setSelectionRange(0, 99999) //For mobile
+    document.execCommand('copy');
 
-  copyText.select();
-  copyText.setSelectionRange(0, 99999) //For mobile
-  document.execCommand('copy');
   }
 };
 
 function whiteColor() {
+
   const color = `rgb(255,255,255)`;
+
   document.body.style.backgroundColor = 'white'
   header.innerHTML = color
+
 }
 
+
+//Events
 circle.addEventListener('mousemove', changeColor);
 circle.addEventListener('mouseout', whiteColor);
 circle.addEventListener('dblclick', copyColor);
